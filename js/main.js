468735a095730e251828d2097a8b3a219c4503f7
catsnotdrugs <catsnotdrugs@icloud.com>
2022-12-09 23:02:29 -0500

    accessibility fixing

--- js/main.js
+++ js/main.js
@@ -21,4 +21,14 @@ document.getElementById("slide-out-nav-menu").classList.add('slide-in-top');
   document.getElementById("slide-out-nav-menu").classList.remove('slide-out-top');
   document.getElementById("slide-out-nav-menu").style.display = "flex";
 }
-
+//
+// function blueTeal() {
+//   document.documentElement.style.setProperty('--background', '#071E3D');
+//     document.documentElement.style.setProperty('--service-div-background', '#131f3460');
+//     document.documentElement.style.setProperty('--button-outline', '#21E6C1');
+//     document.documentElement.style.setProperty('--text', '#21E6C1');
+//     document.documentElement.style.setProperty('--text-light', '#21E6C1');
+//     document.documentElement.style.setProperty('--accent-color', '#21E6C1');
+//     document.documentElement.style.setProperty('--accent-color-2', '#278EA5');
+//     document.documentElement.style.setProperty('--accent-color-3', '#1F4287');
+// }
